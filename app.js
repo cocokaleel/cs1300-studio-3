c0ac084ccedd5debb1724c7b2c44ab65b4ef711c
cocokaleel <cocokaleel@gmail.com>
2022-09-11 17:37:44 -0400

    done

--- app.js
+++ app.js
@@ -26,14 +26,24 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
 
-  // TODO: Create a new HTML element to display your data
+  const filteredArray = fruitsArray.filter(fruit => fruit.nutritions.calories > 50);
+  console.log("filtered array:");
+  console.log(filteredArray);
 
+
+  // TODO: Create a new HTML element to display your data
+  const headerElement = document.getElementById("fruit-info");
   // TODO: Append your new element to the page
+  for (var i = 0; i<filteredArray.length; i++) {
+    const newElement = document.createElement('p');
+    newElement.innerHTML = filteredArray[i].name;
+    headerElement.append(newElement);
+  }
 
 }
 
